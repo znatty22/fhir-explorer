175307fee2ed0eaeb3ec8d40a0275565287f015e
Natasha Singh <dukedesi22@gmail.com>
2023-08-15 20:52:16 -0400

    :art: Add resource count to results display

--- src/components/FhirQueryResults.tsx
+++ src/components/FhirQueryResults.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState, Suspense } from "react";
+import { useState } from "react";
 import JsonView from "react18-json-view";
-import { Loader } from "@/app/loading";
 
 function displayHeaders(headers: any) {
   // Parse response headers and display
@@ -25,46 +24,51 @@ export default function FhirQueryResults({
   data: any;
   statusCode: number | null;
 }) {
+  console.log(JSON.stringify(data));
   return (
-    <Suspense fallback={<Loader />}>
-      <div className="flex flex-col space-y-4">
-        <div className="space-y-2">
-          <div className="flex justify-between items-center">
-            <h3 className="px-2 text-sm text-slate-400 font-light">
-              Response Headers
-            </h3>
-            {!!statusCode && (
-              <h3 className="px-2 text-sm text-slate-400 font-light">
-                HTTP Status Code <span>{statusCode}</span>
-              </h3>
-            )}
-          </div>
-          <div className="w-full h-48 overflow-scroll p-4 bg-white rounded-xl text-sm text-slate-500">
-            <pre>{!!headers && displayHeaders(headers)}</pre>
-          </div>
-        </div>
-        {/* Response Body */}
-        <div className="space-y-2">
+    <div className="flex flex-col space-y-4">
+      <div className="space-y-2">
+        <div className="flex justify-between items-center">
           <h3 className="px-2 text-sm text-slate-400 font-light">
-            Response Body
+            Response Headers
           </h3>
-          {!!data && (
-            <div className="w-full h-[36rem] overflow-scroll p-2 rounded-xl text-slate-500">
-              <div className="text-[16px] rounded-lg p-4 bg-[#2b3f4f]">
-                {typeof data === "object" ? (
-                  <JsonView
-                    collapseObjectsAfterLength={100}
-                    collapseStringsAfterLength={500}
-                    src={data}
-                  />
-                ) : (
-                  <div className="text-white">{data}</div>
-                )}
-              </div>
-            </div>
+          {!!statusCode && (
+            <h3 className="px-2 text-sm text-slate-400 font-light">
+              HTTP Status Code <span>{statusCode}</span>
+            </h3>
           )}
         </div>
+        <div className="w-full h-48 overflow-scroll p-4 bg-white rounded-xl text-sm text-slate-500">
+          <pre>{!!headers && displayHeaders(headers)}</pre>
+        </div>
       </div>
-    </Suspense>
+      {/* Response Body */}
+      <div className="space-y-2">
+        <div className="flex justify-between items-center text-sm text-slate-400 font-light">
+          <h3 className="px-2">Response Body</h3>
+          <h3 className="mx-2">
+            Count:{" "}
+            <span>
+              {typeof data.total === "number" ? data.total : "unknown"}
+            </span>
+          </h3>
+        </div>
+        {!!data && (
+          <div className="w-full h-[36rem] overflow-scroll p-2 rounded-xl text-slate-500">
+            <div className="text-[16px] rounded-lg p-4 bg-[#2b3f4f]">
+              {typeof data === "object" ? (
+                <JsonView
+                  collapseObjectsAfterLength={100}
+                  collapseStringsAfterLength={500}
+                  src={data}
+                />
+              ) : (
+                <div className="text-white">{data}</div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
