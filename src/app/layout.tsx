d816a91c8d51f5b023a1834a2d5fccbd133876b4
Natasha Singh <dukedesi22@gmail.com>
2023-08-16 11:43:12 -0400

    :recycle: Mv navbar to persistent app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
+import Navbar from "@/components/Navbar";
+
 const nunito = localFont({
   src: "./fonts/Nunito-VariableFont_wght.ttf",
   variable: "--font-nunito",
@@ -20,7 +22,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-slate-50">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <div className="bg-slate-50">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 
 import { Loader } from "./loading";
 import { FhirServer, Header, FHIR_SERVERS } from "@/components/Header";
-import Navbar from "@/components/Navbar";
 import FhirQueryForm from "@/components/FhirQueryForm";
 import FhirQueryResults from "@/components/FhirQueryResults";
 
@@ -22,38 +21,32 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-slate-50">
-      <Navbar />
-      <main>
-        {/* Select FHIR Server */}
-        <Header
-          fhirServer={fhirServer}
-          handleServerSelect={handleServerSelect}
-        />
-        {/* Query FHIR Server */}
-        <section id="workspace">
-          <div className="container mx-auto my-10 space-y-8">
-            <FhirQueryForm
-              setLoading={setLoading}
-              fhirServerUrl={new URL(fhirServer.url)}
-              setFhirResponse={setFhirResponse}
+    <main>
+      {/* Select FHIR Server */}
+      <Header fhirServer={fhirServer} handleServerSelect={handleServerSelect} />
+      {/* Query FHIR Server */}
+      <section id="workspace">
+        <div className="container mx-auto my-10 space-y-8">
+          <FhirQueryForm
+            setLoading={setLoading}
+            fhirServerUrl={new URL(fhirServer.url)}
+            setFhirResponse={setFhirResponse}
+          />
+          {!!loading ? (
+            <Loader />
+          ) : !!fhirResponse.data ? (
+            <FhirQueryResults
+              headers={fhirResponse.headers}
+              data={fhirResponse.data}
+              statusCode={fhirResponse.statusCode}
             />
-            {!!loading ? (
-              <Loader />
-            ) : !!fhirResponse.data ? (
-              <FhirQueryResults
-                headers={fhirResponse.headers}
-                data={fhirResponse.data}
-                statusCode={fhirResponse.statusCode}
-              />
-            ) : (
-              <div className="flex justify-center items-center font-light text-xl text-slate-400 h-96 border-2 border-dashed ">
-                No results to display
-              </div>
-            )}
-          </div>
-        </section>
-      </main>
-    </div>
+          ) : (
+            <div className="flex justify-center items-center font-light text-xl text-slate-400 h-96 border-2 border-dashed ">
+              No results to display
+            </div>
+          )}
+        </div>
+      </section>
+    </main>
   );
 }
