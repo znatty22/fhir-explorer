f21d08ba0e1f555bc10d628435653a0b51b2ae6d
Natasha Singh <dukedesi22@gmail.com>
2023-08-21 13:30:34 -0400

    :bug: Only check nextauth url if not prd

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,7 +4,6 @@ const envVars = [
   "AUTH0_CLIENT_ID",
   "AUTH0_CLIENT_SECRET",
   "AUTH0_ISSUER",
-  "NEXTAUTH_URL",
   "FHIR_EXP_PRD_CLIENT_ID",
   "FHIR_EXP_PRD_CLIENT_SECRET",
   "FHIR_EXP_PRD_OIDC_TOKEN_URL",
@@ -15,6 +14,10 @@ const envVars = [
   "FHIR_EXP_DEV_CLIENT_SECRET",
   "FHIR_EXP_DEV_OIDC_TOKEN_URL",
 ];
+
+if (process.env.NODE_ENV !== "production") {
+  envVars.push("NEXTAUTH_URL");
+}
 
 export function getAppEnv() {
   // Check that all env vars are set or throw Error
