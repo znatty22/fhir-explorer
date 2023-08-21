aba64ec17b1c104ed8ca8b7dffc4d613d09041ac
Natasha Singh <dukedesi22@gmail.com>
2023-08-21 11:32:18 -0400

    :recycle: Mv login loader to components dir

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -14,23 +14,7 @@ export default function PageLoader() {
     </div>
   );
 }
-export function LoginLoader() {
-  return (
-    <div className="container mx-auto">
-      <div className="flex justify-center items-center h-screen font-extralight">
-        <div className="flex flex-col justify-center items-center space-y-8">
-          <h1 className="text-2xl text-pink-400">
-            Redirecting you to login ...
-          </h1>
-          <div className="rounded-full ring-pink-400 ring-2 p-2  animate-bounce">
-            <Logo className="fill-pink-400 w-6 h-6" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-export function Loader() {
+export function SectionLoader() {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center font-extralight h-96">
