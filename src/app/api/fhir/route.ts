0b8657fcb87367b5f0be21b270b058d0e930b765
Natasha Singh <dukedesi22@gmail.com>
2023-08-22 17:16:20 -0400

    :recycle: Response gracefully to http method not allowed

--- src/app/api/fhir/route.ts
+++ src/app/api/fhir/route.ts
@@ -97,3 +97,21 @@ export async function POST(request: NextRequest) {
     },
   });
 }
+export async function GET(request: NextRequest) {
+  return NextResponse.json(
+    { error: "unsupported_method", details: "HTTP Method Not Allowed" },
+    { status: 405 }
+  );
+}
+export async function PUT(request: NextRequest) {
+  return NextResponse.json(
+    { error: "unsupported_method", details: "HTTP Method Not Allowed" },
+    { status: 405 }
+  );
+}
+export async function PATCH(request: NextRequest) {
+  return NextResponse.json(
+    { error: "unsupported_method", details: "HTTP Method Not Allowed" },
+    { status: 405 }
+  );
+}
