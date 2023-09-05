90b3c58d85417bdac609ff221915f84ea1061393
Natasha Singh <dukedesi22@gmail.com>
2023-09-05 17:28:38 -0400

    :wrench: Only turn on sentry in prod

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,15 +3,18 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { shouldInitializeSentry } from "@/lib/env";
 
-const SENTRY_DSN = process.env.SENTRY_DSN;
+const NEXT_PUBLIC_SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN,
+if (shouldInitializeSentry()) {
+  Sentry.init({
+    dsn: NEXT_PUBLIC_SENTRY_DSN,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
